b34cd2930cf34488a90a7b9cc1b5afa04757715c
Youngsu Kim <126743125+Createcoding@users.noreply.github.com>
2023-04-10 18:47:31 +0900

    ys커밋

---
+++ src/youngsu/ClientRepository.java
@@ -0,0 +1,22 @@
+package airproject;
+
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class ClientRepository {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        List<Client> clientList = new ArrayList<>();
+
+
+
+    }
+
+
+
+}
