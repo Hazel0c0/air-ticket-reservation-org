ad158b385e891c72317b5684401750d98246eb7b
myblog0419 <myblog0419@naver.com>
2023-04-12 21:02:32 +0900

    "04.12 2일차+1"

--- src/dongwoo/Client.java
+++ src/dongwoo/Client.java
@@ -19,15 +19,11 @@ public class Client {
     private String location;
     private String id;
     private String password;
-    private Card card;
-    private Ticket ticket;
-    private int point;
-    private boolean isAdmin;
+
 
 
     public Client(String userName, Gender gender, String userPhone, String email,
-                  String location, String id, String password, Card card, Ticket ticket,
-                  int point, boolean isAdmin) {
+                  String location, String id, String password) {
         this.userName = userName;
         this.gender = gender;
         this.userPhone = userPhone;
@@ -35,10 +31,63 @@ public class Client {
         this.location = location;
         this.id = id;
         this.password = password;
-        this.card = card;
-        this.ticket = ticket;
-        this.point = point;
-        this.isAdmin = isAdmin;
+
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public Gender getGender() {
+        return gender;
+    }
+
+    public void setGender(Gender gender) {
+        this.gender = gender;
+    }
+
+    public String getUserPhone() {
+        return userPhone;
+    }
+
+    public void setUserPhone(String userPhone) {
+        this.userPhone = userPhone;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
     }
 
     // 회원가입일
--- src/dongwoo/Test.java
+++ src/dongwoo/Test.java
@@ -3,7 +3,7 @@ package dongwoo;
 
 import yougeun.Client.Gender;
 
-public class Test {
+public class Test    {
 
     public static void main(String[] args) {
 
